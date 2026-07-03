9baef0331e8d9105d5a39abb7606b1c62cd565f5
AgentMaxwell <148154260+AgentMaxwell@users.noreply.github.com>
2026-07-03 21:07:29 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// 🔴 REPLACE THIS with your actual Firebase Config
+  const firebaseConfig = {
+    apiKey: "AIzaSyDvtVFsGd37aAWNoLSgAQWxhVa7-gNc1Y8",
+    authDomain: "lqservicetracker.firebaseapp.com",
+    databaseURL: "https://lqservicetracker-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "lqservicetracker",
+    storageBucket: "lqservicetracker.firebasestorage.app",
+    messagingSenderId: "716349093814",
+    appId: "1:716349093814:web:f01b3f17da7c8d0a2f7317",
+    measurementId: "G-DZNMPDJ91K"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the database so other files can import it
+export const db = getFirestore(app);
