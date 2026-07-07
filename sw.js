0e3dfab2761f601f65b48b47f5e27201ebd1ffed
AgentMaxwell <148154260+AgentMaxwell@users.noreply.github.com>
2026-07-07 21:53:16 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pack-tracker-v1.3';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached version or fetch from the network
+      return response || fetch(event.request);
+    })
+  );
+});
